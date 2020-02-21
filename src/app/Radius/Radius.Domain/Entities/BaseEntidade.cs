7d554b7db85538adb9a4b136af08b31334896758
Daniel Aquino <danielaquino91@yahoo.com.br>
2020-02-21 15:27:05 -0300

    Criação de Entidades
    Criação das Classes:
    
    FaleConoscoEntidade
    BaseEntidade

---
+++ src/app/Radius/Radius.Domain/Entities/BaseEntidade.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Radius.Domain.Entities
+{
+    public abstract class BaseEntidade
+    {
+        [Key]
+        public Guid Id { get; set; }
+        private DateTime? _dataCriacao;
+
+        public DateTime? DataCriacao
+        {
+            get { return _dataCriacao; }
+            set { _dataCriacao = (value == null ? DateTime.UtcNow : value); }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+
+        public bool eAtivo { get; set; }
+
+        public void Deletar() { this.eAtivo = false; }
+        public void Ativar() { this.eAtivo = true; }
+        public void GerarId() { this.Id = Guid.NewGuid(); }
+        public void PrepararParaInserir()
+        {
+            Ativar();
+            GerarId();
+            ConfigurarDataCriacao();
+        }
+        public void ConfigurarDataCriacao()
+        {
+            this.DataCriacao = DateTime.UtcNow;
+        }
+        public void ConfigureUpdateAt()
+        {
+            this.UpdateAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/app/Radius/Radius.Domain/Entities/FaleConoscoEntidade.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radius.Domain.Entities
+{
+    public class FaleConoscoEntidade : BaseEntidade
+    {
+        public string Nome
+        {
+            get { return this.Nome; }
+            set { ConfigurarNome(value); }
+        }
+
+        private void ConfigurarNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public string Email
+        {
+            get { return this.Email; }
+            set { ConfigurarEmail(value); }
+        }
+
+        private void ConfigurarEmail(string email)
+        {
+            this.Email = email;
+        }
+
+        public string NomeEmpresa
+        {
+            get { return this.Nome; }
+            set { ConfigurarNomeEmpresa(value); }
+        }
+
+        private void ConfigurarNomeEmpresa(string nomeEmpresa)
+        {
+            this.NomeEmpresa = nomeEmpresa;
+        }
+
+        public string Assunto
+        {
+            get { return this.Assunto; }
+            set { ConfigurarAssunto(value); }
+        }
+
+        private void ConfigurarAssunto(string assunto)
+        {
+            this.Assunto = assunto;
+        }
+
+        public string Mensagem
+        {
+            get { return this.Mensagem; }
+            set { ConfigurarMensagem(value); }
+        }
+
+        private void ConfigurarMensagem(string mensagem)
+        {
+            this.Mensagem = mensagem;
+        }
+    }
+}
