b6ef9e759933c97b72c43b42fcbcc69a5c4d4002
Jorge Lucas <jorge.campos@ivory.com.br>
2020-04-07 08:36:41 -0300

    Entidade funcionario

---
+++ src/app/Radius/Radius.Domain/Entities/FuncionarioEntidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radius.Domain.Entities
+{
+    public class FuncionarioEntidade : BaseEntidade
+    {
+
+        public string Cargo { get; set; }
+        public override bool Validar()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/app/Radius/Radius.Domain/Interfaces/Repositories/IRepositorioFuncionario.cs
@@ -0,0 +1,12 @@
+﻿using Radius.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radius.Domain.Interfaces.Repositories
+{
+    public interface IRepositorioFuncionario : IRepositorio<FuncionarioEntidade>
+    {
+
+    }
+}
